cf4da9d9ef818352dc693e84cefe91ed1652974d
Heisaa <henriksandhall@gmail.com>
2020-03-10 14:12:06 +0100

    dålig optimering

--- ImmediateGeometry.gd
+++ ImmediateGeometry.gd
@@ -2,13 +2,15 @@ extends ImmediateGeometry
 
 export(OpenSimplexNoise) var noise
 const SIZE = 1
-const VERTICES = 25
+const VERTICES = 30
 const Z_DRIFT = 0.001
 
 var yValues = []
-var zOffset = 0
+var zOffset = 1.5
 var grid = []
-var z_sea_speed = 1.5
+var z_sea_speed = 0.1
+var lastZ = VERTICES
+var seaTimer
 
 onready var lines = get_node("../Lines")
 
@@ -16,19 +18,25 @@ onready var lines = get_node("../Lines")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.translate(Vector3(-SIZE * VERTICES / 2, 0, -SIZE * VERTICES / 2))
+	seaTimer = Timer.new()
+	seaTimer.set_wait_time(0.1)
+	seaTimer.connect("timeout", self, "moveSea")
+	add_child(seaTimer)
+	seaTimer.start()
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	yValues = []
 	for z in range(VERTICES + 1):
 		yValues.append([])
 		for x in range(VERTICES + 1):
-			var zTemp = z - zOffset 
-			yValues[z].append(noise.get_noise_2d(x, zTemp) * 10) 
+			#var zTemp = z - zOffset 
+			yValues[z].append(noise.get_noise_2d(x, z) * 10)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
 	
 	#z_sea_speed += lines.angle_x #* Z_DRIFT
-	zOffset += clamp(z_sea_speed, 1, 10) * delta
+	
 	#print(z_sea_speed)
 	
 	self.clear()
@@ -62,4 +70,11 @@ func _process(delta):
 			grid[z].append(secondTri)
 	self.end()
 	
+	
+func moveSea():
+	yValues.append([])
+	lastZ -= zOffset
+	for x in range (VERTICES + 1):
+		yValues[yValues.size() - 1].append(noise.get_noise_2d(x, lastZ) * 10)
+	yValues.pop_front()
 	
